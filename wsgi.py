bdb361a971f698984b39efa9fb5ee1c5ad35b67e
Ivan Bogatyrev <yanagiori.w@gmail.com>
2020-05-14 19:55:21 +0300

    base webhook receiver

---
+++ wsgi.py
@@ -0,0 +1,20 @@
+import cherrypy
+from bot import WebhookServer, config, bot
+
+app = cherrypy.tree.mount(WebhookServer(), '/')
+
+if __name__ == '__main__':
+    bot.remove_webhook()
+    bot.set_webhook(url=config.WEBHOOK_URL_BASE + config.WEBHOOK_URL_PATH,
+                    certificate=open(config.WEBHOOK_SSL_CERT, 'r'))
+
+    cherrypy.config.update({
+        'server.socket_host': config.WEBHOOK_HOST,
+        'server.socket_port': config.WEBHOOK_PORT,
+        'server.ssl_module': 'builtin',
+        'server.ssl_certificate': config.WEBHOOK_SSL_CERT,
+        'server.ssl_private_key': config.WEBHOOK_SSL_PRIV,
+    })
+
+    # Run the application using CherryPy's HTTP Web Server
+    cherrypy.quickstart(WebhookServer())
