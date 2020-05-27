1aba3fa6c1c8cba36066d4ceb7f34aa82c809956
Bogatyrev Ivan <937-bis19u2501@users.noreply.git.iu7.bmstu.ru>
2020-05-27 23:10:07 +0300

    temporarily send messages to me

--- bot/__init__.py
+++ bot/__init__.py
@@ -26,5 +26,5 @@ class WebhookServer(object):
         with open("./bot/bot_settings.json", "r") as f:
             data = json.load(f)
         user_name = data["object_attributes"]["assignee_id"]
-        bot.send_message(message.chat.id, user_name)
+        bot.send_message(87763438, text=user_name)
         return ''
