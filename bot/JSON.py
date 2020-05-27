3615a3ffb9e666e2cf576a667ebf767f2db11b35
Mezherovskiy Alexander <918-mas19u811@users.noreply.git.iu7.bmstu.ru>
2020-05-27 22:58:19 +0300

    Delete JSON.py

--- bot/JSON.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open("./bot/bot_settings.json", "r") as f:
-    data = json.load(f)
-
-user_name = data["object_attributes"]["assignee_id"]
-
-
-bot.send_message(message.chat.id, user_name)
