202acbc32c64c8c19e5bcabb5d9ed0cadc8f6f0f
Ivan Bogatyrev <yanagiori.w@gmail.com>
2020-06-06 06:40:11 +0300

    trying another method

--- bot/__init__.py
+++ bot/__init__.py
@@ -36,6 +36,6 @@ class WebhookServer(object):
                     # для каждого телеграм аккаунта, прикрепленного к этому юзеру
                     print(receiver)
                     bot.send_message(chat_id=receiver['id'],
-                                     text=("Hello! A new merge request is waiting you! \n" + "\n".join(mr.diffs.list())))
+                                     text=("Hello! A new merge request is waiting you! \n" + "\n".join(mr.changes())))
                     # шлем юзеру гит див
 
