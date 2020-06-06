d1a0faa7078327254ebee5f97f5e8d06bd463fc4
Ivan Bogatyrev <yanagiori.w@gmail.com>
2020-06-07 01:24:30 +0300

    fixed file

--- bot/__init__.py
+++ bot/__init__.py
@@ -36,8 +36,8 @@ class WebhookServer(object):
                 result = project.repository_compare(target_branch, source_branch)
                 for receiver in db.token.find({'idGitLab': i['username']}):
                     # для каждого телеграм аккаунта, прикрепленного к этому юзеру
-                    diff = str(file['diff']).replace("```", "")
                     for file in result['diffs']:
+                        diff = str(file['diff']).replace("```", "")
                         message = "Пользователь {0} отправил Вам " \
                                   "запрос на слитие веток {1} и {2} в проекте {3}" \
                                   "\n".format(author_name, target_branch, source_branch, project_name) \
