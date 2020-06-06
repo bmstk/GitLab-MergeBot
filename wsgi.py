9762af25f62a6a8080855e4d0ddcb1ebe24b05ef
Ivan Bogatyrev <yanagiori.w@gmail.com>
2020-06-06 07:02:10 +0300

    more comments

--- wsgi.py
+++ wsgi.py
@@ -11,6 +11,7 @@ from bot.merger_bot import db, timer, bot
 app = cherrypy.tree.mount(WebhookServer(), '/')
 
 if __name__ == '__main__':
+    # конфигурация сервера
     cherrypy.config.update({
         'server.socket_host': config.WEBHOOK_HOST,
         'server.socket_port': config.WEBHOOK_PORT,
@@ -19,6 +20,7 @@ if __name__ == '__main__':
         'server.ssl_private_key': config.WEBHOOK_SSL_PRIV,
     })
 
+    # параллельный запуск бота и сервера
     server_thread = threading.Thread(target=cherrypy.quickstart, args=(WebhookServer(),))
     bot_thread = threading.Thread(target=bot.polling)
     server_thread.start()
