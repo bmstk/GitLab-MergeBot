39d19522f618f421486c5a4377f8cf991532a41a
Ivan Bogatyrev <yanagiori.w@gmail.com>
2020-06-06 19:08:01 +0300

    parsed project name & id, author name and merge request url

--- bot/__init__.py
+++ bot/__init__.py
@@ -12,16 +12,23 @@ class WebhookServer(object):
         # raw_body = cherrypy.request.body.read()
         raw_json = cherrypy.request.json  # получаем вебхук
         if raw_json['object_kind'] == 'merge_request':  # если вебхук вызван мержреквестом
-            print(raw_json)
+            print(raw_json + "\n")
+
             assignees_array = raw_json['assignees']  # находим всем юзеров, заасаненных к мержреквесту
+            project_name = raw_json['project']['name']  # название проекта
+            project_id = raw_json['project']['id']  # id проекта
+            source_branch = raw_json['object_attributes']['source_branch']  # ветка, которую сливаем
+            target_branch = raw_json['object_attributes']['target_branch']  # ветка, в которую сливаем
+            author_name = raw_json['user']['name']  # имя автора merge request
+            merge_request_url = raw_json['object_attributes']['url']  # адрес страницы merge request
+
             for i in assignees_array:  # для каждого пользователя
                 print(i['username'])
                 private_key = db.token.find_one({'idGitLab': i['username']})  # достаем ключ авторизации пользователя
                 # авторизуемся для каждого юзера по последнему токену TODO: оставить только один возможный токен
                 gl = gitlab.Gitlab('https://git.iu7.bmstu.ru/', private_token=private_key['token'][-1])
                 project = gl.projects.get(raw_json['project']['id'])  # находим проект
-                source_branch = raw_json['object_attributes']['source_branch']
-                target_branch = raw_json['object_attributes']['target_branch']
+
                 print(project.mergerequests.list(state='merged', order_by='updated_at'),
                       raw_json['object_attributes']['iid'] + '\n')
 
