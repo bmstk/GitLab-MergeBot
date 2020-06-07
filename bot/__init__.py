4dd043e3a0581c914ff78877bfbdb0c6836fc8eb
Ivan Bogatyrev <yanagiori.w@gmail.com>
2020-06-07 05:24:57 +0300

    updated comment

--- bot/__init__.py
+++ bot/__init__.py
@@ -24,7 +24,7 @@ class WebhookServer(object):
             author_name = raw_json['user']['name']  # имя автора merge request
             merge_request_url = raw_json['object_attributes']['url']  # адрес страницы merge request
             mg_title = raw_json['object_attributes']['title']  # заголовок мерд реквеста
-            action = raw_json['object_attributes']['action']  # проверяем, были ли обновления до этого
+            action = raw_json['object_attributes']['action']  # действие
 
             ##########################################################################################
 
