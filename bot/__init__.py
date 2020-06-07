d97d2eb971cfe8b3f5f93ac8b288c8e2c34b43d1
Ivan Bogatyrev <yanagiori.w@gmail.com>
2020-06-07 21:42:09 +0300

    type error fix

--- bot/__init__.py
+++ bot/__init__.py
@@ -57,7 +57,7 @@ class WebhookServer(object):
                             message = "Merge request {0} был закрыт.".format(mg_title)
                             bot.send_message(chat_id=decoder(key, receiver['id']), text=message)
 
-                        if (action == 'update' or action == 'close') and i >= 1 and len(result['diffs'] - 3) != 0:
+                        if (action == 'update' or action == 'close') and i >= 1 and len(result['diffs']) - 1 != 0:
                             message = "А так же еще {0} изменений".format(len(result['diffs']) - 3)
                             bot.send_message(chat_id=decoder(key, receiver['id']), text=message)
                             break
